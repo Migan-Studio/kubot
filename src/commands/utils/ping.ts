84a8bc4f7a45926ab5660ea69e0ac28d25e64e50
Migan178 <tigersiwoo131@gmail.com>
2021-11-04 21:29:14 +0900

    feat: Message Ping

--- src/commands/utils/ping.ts
+++ src/commands/utils/ping.ts
@@ -1,26 +1,49 @@
 import { MessageCommand } from 'discommand'
-import { Message, MessageEmbed } from 'discord.js'
+import { Formatters, Message, MessageEmbed } from 'discord.js'
 
 export = class extends MessageCommand {
   name = '핑'
   aliases = ['ping']
   execute(msg: Message, args: string[]) {
-    msg.reply({
-      embeds: [
-        new MessageEmbed()
-          .setColor(0x459ff7)
-          .setTitle('퐁!')
-          .addFields({
-            name: '핑',
-            value: `\`${msg.client.ws.ping}ms\``,
-            inline: true,
-          })
-          .setTimestamp(Date.now())
-          .setFooter(
-            msg.author.tag,
-            msg.author.displayAvatarURL({ dynamic: true, size: 512 })
+    const embed = new MessageEmbed()
+      .setColor(0x459ff7)
+      .setTitle('퐁!')
+      .setTimestamp(Date.now())
+      .setFooter(
+        msg.author.tag,
+        msg.author.displayAvatarURL({ dynamic: true, size: 512 })
+      )
+    msg
+      .reply({
+        embeds: [
+          embed.setDescription(
+            Formatters.codeBlock(
+              'markdown',
+              `# 메세지 핑
+측정중...
+        
+# 웹소켓 핑
+측정중...`
+            )
           ),
-      ],
-    })
+        ],
+      })
+      .then(message =>
+        message.edit({
+          embeds: [
+            embed.setDescription(
+              Formatters.codeBlock(
+                'markdown',
+                `# 메세지 핑
+${message.createdTimestamp - msg.createdTimestamp}ms
+
+# 웹소켓 핑
+${msg.client.ws.ping}ms`
+              )
+            ),
+          ],
+        })
+      )
+      .catch(console.error)
   }
 }
--- src/slashCommands/utils/ping.ts
+++ src/slashCommands/utils/ping.ts
@@ -1,5 +1,5 @@
 import { SlashCommand } from 'discommand-slash'
-import { CommandInteraction, MessageEmbed } from 'discord.js'
+import { CommandInteraction, MessageEmbed, Formatters } from 'discord.js'
 import { SlashCommandBuilder } from '@discordjs/builders'
 
 export = class extends SlashCommand {
@@ -7,21 +7,26 @@ export = class extends SlashCommand {
     .setName('ping')
     .setDescription('이봇의 핑을 반환합니다.')
   execute(interaction: CommandInteraction) {
+    const embed = new MessageEmbed()
+      .setColor(0x459ff7)
+      .setTitle('퐁!')
+      .setTimestamp(Date.now())
+      .setFooter(
+        interaction.user.tag,
+        interaction.user.displayAvatarURL({ dynamic: true, size: 512 })
+      )
     interaction.reply({
       embeds: [
-        new MessageEmbed()
-          .setColor(0x459ff7)
-          .setTitle('퐁!')
-          .addFields({
-            name: '핑',
-            value: `\`${interaction.client.ws.ping}ms\``,
-            inline: true,
-          })
-          .setTimestamp(Date.now())
-          .setFooter(
-            interaction.user.tag,
-            interaction.user.displayAvatarURL({ dynamic: true, size: 512 })
-          ),
+        embed.setDescription(
+          Formatters.codeBlock(
+            'markdown',
+            `# 인터렉션 핑
+지금은 안돼요ㅜㅜ 나중에 할수 있을꺼에요...
+    
+# 웹소켓 핑
+${interaction.client.ws.ping}`
+          )
+        ),
       ],
     })
   }
