1fd58977fe42e39726f416e40e2b327b24cab01a
Migan178 <migan9857@gmail.com>
2022-01-30 21:18:46 +0900

    BREAKING CHANGE: remove interaction ping.

--- src/slashCommands/utils/ping.ts
+++ src/slashCommands/utils/ping.ts
@@ -1,12 +1,17 @@
 import { SlashCommand } from 'discommand-slash'
-import { CommandInteraction, MessageEmbed, Formatters } from 'discord.js'
+import {
+  CommandInteraction,
+  MessageEmbed,
+  Formatters,
+  Interaction,
+} from 'discord.js'
 import { SlashCommandBuilder } from '@discordjs/builders'
 
 export = class extends SlashCommand {
   data = new SlashCommandBuilder()
     .setName('ping')
     .setDescription('이봇의 핑을 반환합니다.')
-  async execute(interaction: CommandInteraction) {
+  execute(interaction: CommandInteraction) {
     const embed = new MessageEmbed()
       .setColor(0x459ff7)
       .setTitle('퐁!')
@@ -15,9 +20,7 @@ export = class extends SlashCommand {
         interaction.user.tag,
         interaction.user.displayAvatarURL({ dynamic: true, size: 512 })
       )
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const tempInteraction: CommandInteraction = await interaction.reply({
+    interaction.reply({
       embeds: [
         embed.setDescription(
           Formatters.codeBlock(
@@ -31,15 +34,12 @@ export = class extends SlashCommand {
         ),
       ],
     })
-    await interaction.editReply({
+    interaction.editReply({
       embeds: [
         embed.setDescription(
           Formatters.codeBlock(
             'markdown',
-            `# 인터렉션 핑
-${tempInteraction.createdTimestamp - interaction.createdTimestamp}
-
-# 웹소켓 핑
+            `# 웹소켓 핑
 ${interaction.client.ws.ping}`
           )
         ),
