f88f5c3b23d16f8769bae98573452140946d7d11
Migan178 <migan9857@gmail.com>
2022-01-30 20:59:09 +0900

    feat: interaction ping

--- src/slashCommands/utils/ping.ts
+++ src/slashCommands/utils/ping.ts
@@ -6,7 +6,7 @@ export = class extends SlashCommand {
   data = new SlashCommandBuilder()
     .setName('ping')
     .setDescription('이봇의 핑을 반환합니다.')
-  execute(interaction: CommandInteraction) {
+  async execute(interaction: CommandInteraction) {
     const embed = new MessageEmbed()
       .setColor(0x459ff7)
       .setTitle('퐁!')
@@ -15,14 +15,30 @@ export = class extends SlashCommand {
         interaction.user.tag,
         interaction.user.displayAvatarURL({ dynamic: true, size: 512 })
       )
-    interaction.reply({
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const tempInteraction: CommandInteraction = await interaction.reply({
       embeds: [
         embed.setDescription(
           Formatters.codeBlock(
             'markdown',
             `# 인터렉션 핑
-지금은 안돼요ㅜㅜ 나중에 할수 있을꺼에요...
+측정중...
     
+# 웹소켓 핑
+측정중...`
+          )
+        ),
+      ],
+    })
+    await interaction.editReply({
+      embeds: [
+        embed.setDescription(
+          Formatters.codeBlock(
+            'markdown',
+            `# 인터렉션 핑
+${tempInteraction.createdTimestamp - interaction.createdTimestamp}
+
 # 웹소켓 핑
 ${interaction.client.ws.ping}`
           )
