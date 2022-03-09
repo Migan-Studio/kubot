693477af40dbad52b4f5bc49a29786b4a68caf6d
Migan178 <migan9857@gmail.com>
2022-03-09 22:08:35 +0900

    .

--- src/Commands/developer/announcement.ts
+++ src/Commands/developer/announcement.ts
@@ -1,5 +1,11 @@
 import { SlashCommand } from 'discommand-slash'
-import { CommandInteraction, MessageEmbed, TextChannel } from 'discord.js'
+import {
+  CommandInteraction,
+  MessageEmbed,
+  TextChannel,
+  User,
+  Team,
+} from 'discord.js'
 import { SlashCommandBuilder } from '@discordjs/builders'
 
 export = class extends SlashCommand {
@@ -14,7 +20,14 @@ export = class extends SlashCommand {
         .setRequired(true)
     })
   execute(interaction: CommandInteraction) {
-    if (interaction.user.id !== interaction.client.ownerID)
+    function getOwnerID() {
+      if (interaction.client.application?.owner instanceof Team) {
+        return interaction.client.application!.owner!.ownerId!
+      } else if (interaction.client.application?.owner instanceof User) {
+        return interaction.client.application!.owner!.id!
+      }
+    }
+    if (interaction.user.id !== getOwnerID())
       return interaction.reply({
         content: '이 명령어는 봇 개발자 전용이에요',
         ephemeral: true,
@@ -22,7 +35,11 @@ export = class extends SlashCommand {
     interaction.client.channels.cache.forEach(channel => {
       const textChannel = channel as TextChannel
       if (textChannel.type !== 'GUILD_TEXT') return
-      if (textChannel.topic?.includes('kubot-공지')) {
+      if (
+        textChannel.topic?.includes(
+          `${interaction.client.user?.username.toLowerCase()}-공지`
+        )
+      ) {
         textChannel.send({
           embeds: [
             new MessageEmbed()
@@ -32,13 +49,18 @@ export = class extends SlashCommand {
               )
               .setColor(0x459ff7)
               .setTimestamp(Date.now())
-              .setFooter(
-                `공지 발송자: ${interaction.user.tag}`,
-                interaction.user.displayAvatarURL()
-              ),
+              .setAuthor({
+                name: `공지 작성자: ${interaction.user.tag}`,
+                iconURL: interaction.user.displayAvatarURL(),
+              }),
           ],
         })
-      } else if (!textChannel.topic?.includes('kubot-공지')) return
+      } else if (
+        !textChannel.topic?.includes(
+          `${interaction.client.user?.username.toLowerCase()}-공지`
+        )
+      )
+        return
     })
     interaction.reply({
       content: `성공적으로 ${interaction.options.getString(
